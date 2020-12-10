896db7d67e65027a828c81c6e28aa8d2844ecb2e
Afish716 <1409152793@qq.com>
2020-12-10 16:36:07 +0800

    初始化仓库

---
+++ cmake-build-debug/CMakeFiles/linear_algebra.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linear_algebra.dir/main.cpp.obj"
+  "liblinear_algebra.dll.a"
+  "linear_algebra.exe"
+  "linear_algebra.exe.manifest"
+  "linear_algebra.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linear_algebra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ readme.md
@@ -0,0 +1,5 @@
+#Created By Guoheng Yu 
+##E-mail: Guoheng.yu@outlook.com
+**This is a library created for my linear algebra calculate**  
+**As we all known,linear algerbra is based on martix. So the**  
+**foundation of this library is *martix* class**
